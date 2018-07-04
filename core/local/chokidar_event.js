d7348c52876837e8de015f83b07769da0c0a11d4
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-07-04 16:56:24 +0200

    core/local: Introduce ChokidarEventType type

--- core/local/chokidar_event.js
+++ core/local/chokidar_event.js
@@ -3,6 +3,13 @@
 /*::
 import type fs from 'fs'
 
+export type ChokidarEventType =
+  | 'add'
+  | 'addDir'
+  | 'change'
+  | 'unlink'
+  | 'unlinkDir'
+
 export type ChokidarAdd = {type: 'add', path: string, stats: fs.Stats}
 export type ChokidarAddDir = {type: 'addDir', path: string, stats: fs.Stats}
 export type ChokidarChange = {type: 'change', path: string, stats: fs.Stats}
@@ -21,7 +28,7 @@ module.exports = {
   build
 }
 
-function build (type /*: string */, path /*: ?string */, stats /*: ?fs.Stats */) /*: ChokidarEvent */ {
+function build (type /*: ChokidarEventType */, path /*: ?string */, stats /*: ?fs.Stats */) /*: ChokidarEvent */ {
   const event /*: Object */ = {type}
   if (path != null) event.path = path
   if (stats != null) event.stats = stats
